29f474d653527862db0e37ad6f36c417cc44a636
Orizu Nwokeji <arpa101@yahoo.co.uk>
2016-09-01 19:48:58 +0100

    Article metadata display feature into a component

---
+++ src/js/components/article-helpers/article-meta.component.js
@@ -0,0 +1,12 @@
+/**
+ * Created by orizu on 01/09/2016.
+ */
+let ArticleMeta= {
+  bindings: {
+    article: '=',
+  },
+  transclude: true,
+  templateUrl: 'components/article-helpers/article-meta.html'
+};
+
+export default ArticleMeta;
